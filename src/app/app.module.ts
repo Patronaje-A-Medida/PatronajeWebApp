a14dc12c94f7f7246b7159ebbbecf3ec0480e096
Jeiel <jeiel.disas@gmail.com>
2021-11-11 19:54:11 -0500

    estructura de carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentExampleComponent } from './components/component-example/component-example.component';
+import { PageExampleComponent } from './pages/page-example/page-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentExampleComponent,
+    PageExampleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/model-example.spec.ts
@@ -0,0 +1,7 @@
+import { ModelExample } from './model-example';
+
+describe('ModelExample', () => {
+  it('should create an instance', () => {
+    expect(new ModelExample()).toBeTruthy();
+  });
+});
