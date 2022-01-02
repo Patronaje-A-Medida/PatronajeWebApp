bd07e08762645fc14d1defbb33462a2aedd87260
Jeiel <jeiel.disas@gmail.com>
2022-01-02 13:19:13 -0500

    #10 mejora de reg expression para contraseñas

--- src/app/core/utils/regex.constants.ts
+++ src/app/core/utils/regex.constants.ts
@@ -1,4 +1,5 @@
-export const RGXP_STRONG_PASSWORD = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_.,;]).{8,}$/;
-export const RGXP_MID_PASSWORD = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
-export const RGXP_WEAK_PASSWORD = /^(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
+export const RGXP_STRONG_PASSWORD = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*\-_.,;]).{8,20}$/;
+export const RGXP_MID_PASSWORD = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,20}$/;
+export const RGXP_WEAK_PASSWORD = /^(?=.*?[a-z])(?=.*?[0-9]).{8,20}$/;
+export const RGXP_MID_PASSWORD_2 = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*\-_.,;]).{8,20}$/;
 
