508bc82f8a1bc9892e756c2a1342ebbad2657edf
Jeiel <jeiel.disas@gmail.com>
2022-01-03 22:59:43 -0500

    Close #12 creacion de auth guard para proteccion de vistas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {OrdersComponent} from './pages/orders/orders.component';
 import {OrderDetailsComponent} from './pages/order-details/order-details.component';
 import { BaseLayoutComponent } from './shared/layouts/base-layout/base-layout.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   /*{
@@ -16,12 +17,20 @@ const routes: Routes = [
     path: '', 
     component: BaseLayoutComponent,
     children: [
-      {path: '', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule)}
+      { path: '', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) }
     ]
   },
-  {path: 'orders', component: OrdersComponent},
-  {path: 'orders/:id', component: OrderDetailsComponent},
-  {path: '**', redirectTo: ''}
+  {
+    path: '',
+    component: BaseLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) }
+    ]
+  },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'orders/:id', component: OrderDetailsComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
