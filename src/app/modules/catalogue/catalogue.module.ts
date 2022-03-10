394fe164020ea0461b43532ed37285683846e692
Jeiel <jeiel.disas@gmail.com>
2022-03-10 02:57:21 -0500

    #20 creacion de vista de garment-details

--- src/app/modules/catalogue/catalogue.module.ts
+++ src/app/modules/catalogue/catalogue.module.ts
@@ -5,11 +5,15 @@ import { CatalogueRoutingModule } from './catalogue-routing.module';
 import { CatalogueComponent } from './pages/catalogue/catalogue.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule } from '@angular/forms';
+import { GarmentCardComponent } from './components/garment-card/garment-card.component';
+import { GarmentDetailsComponent } from './pages/garment-details/garment-details.component';
 
 
 @NgModule({
   declarations: [
-    CatalogueComponent
+    CatalogueComponent,
+    GarmentCardComponent,
+    GarmentDetailsComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/catalogue/pages/garment-details/garment-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-garment-details',
+  templateUrl: './garment-details.component.html',
+  styleUrls: ['./garment-details.component.scss']
+})
+export class GarmentDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
