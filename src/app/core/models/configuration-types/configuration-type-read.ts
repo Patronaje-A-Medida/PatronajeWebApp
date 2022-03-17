6074dcec09c749f39e77ec1619d31c8345305707
Jeiel <jeiel.disas@gmail.com>
2022-03-17 02:14:34 -0500

    #22 update de modelos configuration-type y mejora de configuration-type service

--- src/app/core/models/configuration-types/configuration-type-read.ts
+++ src/app/core/models/configuration-types/configuration-type-read.ts
@@ -3,4 +3,6 @@ import { TypeRead } from './type-read';
 export interface ConfigurationTypeRead {
   categories: TypeRead[];
   orderStatus: TypeRead[];
+  fabrics: TypeRead[];
+  occasions: TypeRead[];
 }
