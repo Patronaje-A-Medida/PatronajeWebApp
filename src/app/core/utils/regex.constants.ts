2f199d7d881db9e211ec7bbbf61c1a9d75d39cf8
Jeiel <jeiel.disas@gmail.com>
2022-03-17 02:17:35 -0500

    #22 agregado nuevas utilidades de enums y funciones para re-escalado de imagenes

--- src/app/core/utils/regex.constants.ts
+++ src/app/core/utils/regex.constants.ts
@@ -2,4 +2,5 @@ export const RGXP_STRONG_PASSWORD = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?
 export const RGXP_MID_PASSWORD = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,20}$/;
 export const RGXP_WEAK_PASSWORD = /^(?=.*?[a-z])(?=.*?[0-9]).{8,20}$/;
 export const RGXP_MID_PASSWORD_2 = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*\-_.,;]).{8,20}$/;
+export const RGXP_NUMBER_PRICE = /^(?!0*[.,]0*$|[.,]0*$|0*$)\d+[,.]?\d{0,2}$/;
 
