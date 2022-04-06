5bb73dee28d8d66a09623bcdd7892043c5fcb544
Jeiel <jeiel.disas@gmail.com>
2022-04-06 11:34:49 -0500

    #27 cambio temporal del enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,10 @@
 export const environment = {
   production: false,
   apiUrl: '*',
-  apiAuthUrl: 'http://localhost:5000/api/v1',
-  apiGestionUrl: 'http://localhost:5010/api/v1',
+  /*apiAuthUrl: 'http://localhost:5000/api/v1',
+  apiGestionUrl: 'http://localhost:5010/api/v1',*/
+  apiAuthUrl: 'https://autenticacion-service.azurewebsites.net/api/v1',
+  apiGestionUrl: 'https://gestion-service.azurewebsites.net/api/v1',
 };
 
 /*
